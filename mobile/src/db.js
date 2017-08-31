fece49773025ddbb836d0c1aa5a2a7cba22fae12
Shiki <jayson@basanes.net>
2017-08-31 08:33:05 -0600

    Mobile: Add schema for `Stock` and `Alert`

--- mobile/src/db.js
+++ mobile/src/db.js
@@ -9,11 +9,47 @@ User.schema = {
     uuid: { type: 'string' },
     apns_key: { type: 'string', optional: true },
     created_at: { type: 'date' },
+    updated_at: { type: 'date' },
+    synchronized: { type: 'bool', default: false }
+  }
+}
+
+class Stock {}
+
+Stock.schema = {
+  name: 'Stock',
+  primaryKey: 'symbol',
+  properties: {
+    symbol: { type: 'string' },
+    as_of: { type: 'date' },
+    price: { type: 'double' },
+    percent_change: { type: 'double' },
+    created_at: { type: 'date' },
     updated_at: { type: 'date' }
   }
 }
 
-const schema = [User]
+class Alert {}
+
+Alert.schema = {
+  name: 'Alert',
+  primaryKey: 'uuid',
+  properties: {
+    uuid: { type: 'string' },
+    user: { type: 'User' },
+    stock: { type: 'Stock' },
+    operator: { type: 'string' },
+    price: { type: 'double' },
+    notes: { type: 'string' },
+    triggered: { type: 'bool', default: false },
+    created_at: { type: 'date' },
+    updated_at: { type: 'date' },
+    is_deleted: { type: 'bool', default: false },
+    synchronized: { type: 'bool', default: false }
+  }
+}
+
+const schema = [User, Stock, Alert]
 
 export const db = {}
 
